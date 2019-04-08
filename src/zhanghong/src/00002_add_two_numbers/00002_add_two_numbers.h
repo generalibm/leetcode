0a5491a576577b6e0ffc277804829460a88e364d
zhanghong <zhanghong@travel-x.cn>
2019-04-08 17:34:23 +0800

    [ADD] medium problem add two numbers with single linked list
    之前：
    之后：

---
+++ src/zhanghong/src/00002_add_two_numbers/00002_add_two_numbers.h
@@ -0,0 +1,11 @@
+#ifndef SRC_ZHANGHONG_SRC_00002_ADD_TWO_NUMBERS
+#define SRC_ZHANGHONG_SRC_00002_ADD_TWO_NUMBERS
+
+struct ListNode;
+
+class Solution {
+public:
+	ListNode* addTwoNumbers(ListNode * l1, ListNode* l2);
+};
+
+#endif
---
+++ src/zhanghong/src/00002_add_two_numbers/00002_add_two_numbers_test.cc
@@ -0,0 +1,122 @@
+#include <gtest/gtest.h>
+
+#include "00002_add_two_numbers.h"
+
+#include <string>
+using namespace std;
+
+struct ListNode
+{
+	int val;
+	struct ListNode * next;
+
+	ListNode(int x):val(x),next(NULL){}
+};
+
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+string listNodeToString(ListNode* node) {
+    if (node == nullptr) {
+        return "[]";
+    }
+
+    string result;
+    while (node) {
+        result += to_string(node->val) + ", ";
+        node = node->next;
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+//int main() {
+//    string line;
+//    while (getline(cin, line)) {
+//        ListNode* l1 = stringToListNode(line);
+//        getline(cin, line);
+//        ListNode* l2 = stringToListNode(line);
+//
+//        ListNode* ret = Solution().addTwoNumbers(l1, l2);
+//
+//        string out = listNodeToString(ret);
+//        cout << out << endl;
+//    }
+//    return 0;
+//}
+
+
+
+TEST(add_two_numbers, same_length_integers)
+{
+	std::string line1 = "[2,4,3]";
+	std::string line2 = "[5,6,4]";
+	ListNode *l1 = stringToListNode(line1);
+	ListNode *l2 = stringToListNode(line2);
+
+	Solution solution;
+	ListNode * ret = solution.addTwoNumbers(l1, l2);
+	std::string out = listNodeToString(ret);
+    EXPECT_EQ("[7, 0, 8]", out);
+
+    delete l1;
+    delete l2;
+    delete ret;
+}
+
+TEST(add_two_numbers, diff_length_integers)
+{
+	std::string line1 = "[2,4]";
+	std::string line2 = "[5,6,4]";
+	ListNode *l1 = stringToListNode(line1);
+	ListNode *l2 = stringToListNode(line2);
+
+	Solution solution;
+	ListNode * ret = solution.addTwoNumbers(l1, l2);
+	std::string out = listNodeToString(ret);
+    EXPECT_EQ("[8, 8, 5]", out);
+
+	delete l1;
+	delete l2;
+	delete ret;
+}
