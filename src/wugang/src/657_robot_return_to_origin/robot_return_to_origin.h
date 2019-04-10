9f06198d8aa8bc98fdc380d8edb7937d0d1c587f
wugang <wugang@travel-x.cn>
2019-04-10 10:07:28 +0800

    657 Robot Return to Origin

---
+++ src/wugang/src/657_robot_return_to_origin/robot_return_to_origin.h
@@ -0,0 +1,14 @@
+#ifndef ROBOT_RETURN_TO_ORIGIN_H_
+#define ROBOT_RETURN_TO_ORIGIN_H_
+
+#include <string>
+
+using namespace std;
+
+class RobotReturnToOrigin
+{
+public:
+    bool judgeCircle(string moves);	
+};
+
+#endif
