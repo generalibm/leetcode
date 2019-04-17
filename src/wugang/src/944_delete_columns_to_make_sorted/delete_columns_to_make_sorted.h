9eff5d000c50eab6a734f30c3151869e427853a0
wugang <wugang@travel-x.cn>
2019-04-17 10:59:53 +0800

    [ADD] 944 delete columns to make sorted
    before:none
    after:none

---
+++ src/wugang/src/944_delete_columns_to_make_sorted/delete_columns_to_make_sorted.h
@@ -0,0 +1,16 @@
+#ifndef WG_DELETE_COLUMNS_TO_MAKE_SORTED_H_
+#define WG_DELETE_COLUMNS_TO_MAKE_SORTED_H_
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class DeleteColumnsToMakeSorted
+{
+public:
+    int minDeletionSize(vector<string>& A);
+
+};
+
+#endif
