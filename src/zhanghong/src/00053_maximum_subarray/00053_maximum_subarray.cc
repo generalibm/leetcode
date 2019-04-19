f37669eb8e7135093d82a17ad0e87c669d372f26
zhanghong <zhanghong@travel-x.cn>
2019-04-19 17:23:46 +0800

    [MOD] 优化分治算法中不必要的计算
    之前：
    之后：

--- src/zhanghong/src/00053_maximum_subarray/00053_maximum_subarray.cc
+++ src/zhanghong/src/00053_maximum_subarray/00053_maximum_subarray.cc
@@ -26,76 +26,73 @@
 //    return max;
 //}
 
-// dived-conquer Approach
+// divide-conquer Approach
 // Time:  O(n*log(n))
 // Space: O(1) + recursive stack consume
-//int Solution::maxSubArray(const vector<int>& nums)
-//{
-//    if (nums.empty())
-//        return 0;
-//    if (nums.size() == 1)
-//        return nums.front();
-//
-//    size_t low = 0;
-//    size_t high = nums.size() - 1;
-//    size_t mid = (low + high) / 2;
-//
-//    int sum_left = 0, max_left = nums[mid];
-//    for (int i = mid; i >= 0; i--)
-//    {
-//        sum_left += nums[i];
-//        if (max_left < sum_left)
-//            max_left = sum_left;
-//    }
-//
-//    int sum_right = 0, max_right = nums[mid];
-//    for (size_t j = mid; j <= high; j++)
-//    {
-//        sum_right += nums[j];
-//        if (max_right < sum_right)
-//            max_right = sum_right;
-//    }
-//
-//    int sum = max_left + max_right - nums[mid];
-//
-//    vector<int> left(mid - low + 1);
-//    for (size_t i = 0; i <= mid; i++)
-//    {
-//        left[i] = nums[i];
-//    }
-//
-//    vector<int> right(high - mid);
-//    for (size_t i = 0, j = mid + 1; j <= high; i++, j++)
-//    {
-//        right[i] = nums[j];
-//    }
-//
-//    std::initializer_list<int> res = { maxSubArray(left), maxSubArray(right),
-//            sum };
-//
-//    return std::max(res);
-//}
+int Solution::maxSubArray(const vector<int>& nums)
+{
+    if (nums.empty())
+        return 0;
+    if (nums.size() == 1)
+        return nums.front();
+
+    size_t low = 0;
+    size_t high = nums.size() - 1;
+    size_t mid = (low + high) / 2;
+
+    int sum_left = 0, max_left = nums[mid-1];
+    for (int i = mid-1; i >= 0; i--)
+    {
+        sum_left += nums[i];
+        if (max_left < sum_left)
+            max_left = sum_left;
+    }
+
+    int sum_right = 0, max_right = nums[mid+1];
+    for (size_t j = mid+1; j <= high; j++)
+    {
+        sum_right += nums[j];
+        if (max_right < sum_right)
+            max_right = sum_right;
+    }
+
+    int sum = max_left + max_right + nums[mid];
+
+    vector<int> left(mid - low + 1);
+    for (size_t i = 0; i <= mid; i++)
+    {
+        left[i] = nums[i];
+    }
+
+    vector<int> right(high - mid);
+    for (size_t i = 0, j = mid + 1; j <= high; i++, j++)
+    {
+        right[i] = nums[j];
+    }
+
+    return std::max(std::max(maxSubArray(left), maxSubArray(right)), sum);
+}
 
 // Dynamic programming Approach
 // Time:  O(n)
 // Space: O(n)
-int Solution::maxSubArray(const vector<int>& nums)
-{
-    if (nums.empty()) return 0;
-    if (nums.size() == 1) return nums.front();
-
-    vector<int> dp(nums.size());
-    int res = dp[0] = nums[0];
-
-    for (size_t i = 1; i < nums.size(); i++)
-    {
-        dp[i] = nums[i] + (dp[i-1] > 0 ? dp[i-1] : 0);
-
-        res = std::max(dp[i], res);
-    }
-
-    return res;
-}
+//int Solution::maxSubArray(const vector<int>& nums)
+//{
+//    if (nums.empty()) return 0;
+//    if (nums.size() == 1) return nums.front();
+//
+//    vector<int> dp(nums.size());
+//    int res = dp[0] = nums[0];
+//
+//    for (size_t i = 1; i < nums.size(); i++)
+//    {
+//        dp[i] = nums[i] + (dp[i-1] > 0 ? dp[i-1] : 0);
+//
+//        res = std::max(dp[i], res);
+//    }
+//
+//    return res;
+//}
 
 // Prefix array Approach
 // Time:  O(n)
