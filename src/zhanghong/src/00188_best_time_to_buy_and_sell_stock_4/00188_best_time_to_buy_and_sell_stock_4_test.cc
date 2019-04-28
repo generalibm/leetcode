9be4c509771fe1d3dc371e5c0d9030980aed8556
zhanghong <zhanghong@travel-x.cn>
2019-04-28 18:48:12 +0800

    [MOD] Pass all test cases with dynamic programming approch, O(n), O(n)
    before:
    after:

--- src/zhanghong/src/00188_best_time_to_buy_and_sell_stock_4/00188_best_time_to_buy_and_sell_stock_4_test.cc
+++ src/zhanghong/src/00188_best_time_to_buy_and_sell_stock_4/00188_best_time_to_buy_and_sell_stock_4_test.cc
@@ -18,3 +18,16 @@ TEST_F(SolutionTest188, Example2)
 {
     EXPECT_EQ(7, sol.maxProfit(2, { 3, 2, 6, 5, 0, 3 }));
 }
+
+TEST_F(SolutionTest188, StackOverFlow1)
+{
+    EXPECT_EQ(0, sol.maxProfit(0, { 1, 3 }));
+}
+
+TEST_F(SolutionTest188, TimeLimitExceeded1)
+{
+EXPECT_EQ(482, sol.maxProfit(11,
+                {48,12,60,93,97,42,25,64,17,56,85,93,9,48,52,42,58,85,81,84,69,36,1,54,23,15,72,15,11,94}))
+;
+}
+
