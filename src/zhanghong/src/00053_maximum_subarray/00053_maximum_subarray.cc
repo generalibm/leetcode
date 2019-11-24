1fc1c66ddfdb214c9da718810230a22cdcd554db
zh <generalibm@foxmail.com>
2019-11-24 10:22:02 +0800

    [MOD] add another form to DP formula
    before:
    after: this would make it easy to understand to optimise to Prefix Approach.

--- src/zhanghong/src/00053_maximum_subarray/00053_maximum_subarray.cc
+++ src/zhanghong/src/00053_maximum_subarray/00053_maximum_subarray.cc
@@ -87,7 +87,7 @@ int Solution::maxSubArray(const vector<int>& nums)
 //    for (size_t i = 1; i < nums.size(); i++)
 //    {
 //        dp[i] = nums[i] + (dp[i-1] > 0 ? dp[i-1] : 0);
-//
+//        // dp[i] = std::max(dp[i-1]+nums[i], nums[i]);
 //        res = std::max(dp[i], res);
 //    }
 //
